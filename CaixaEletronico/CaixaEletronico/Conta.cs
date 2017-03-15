e1ed6db1622b1e8c34db3c8c44a20193b7adf05e
Demétrios Miguel <demetriosmiguel@hotmail.com>
2017-03-15 17:56:30 -0300

    Extraindo comportamentos: métodos

---
+++ CaixaEletronico/CaixaEletronico/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    public class Conta
+    {
+        public int numero;
+        public String titular;
+        public double saldo;
+
+        public void Sacar(double valor)
+        {
+            this.saldo -= valor;
+        }
+
+        public void Deposita (double valor)
+        {
+
+            this.saldo += valor;
+        }
+
+        public void Traferencia ( double valor , Conta destino)
+        {
+
+            this.Sacar(valor);
+            destino.Deposita(valor);
+
+        }
+
+    }
+}
---
+++ CaixaEletronico/CaixaEletronico/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Conta guilerme = new Conta();
+            Conta mauricio = new Conta();
+
+            guilerme.saldo = 1500.0;
+            mauricio.saldo = 2000.0;
+
+
+            guilerme.Traferencia(200.0, mauricio);
+
+            MessageBox.Show("O saldo do guilerme é " + guilerme.saldo);
+            MessageBox.Show("O saldo do guilerme é " + mauricio.saldo);
+
+
+
+
+
+
+
+        }
+    }
+}
